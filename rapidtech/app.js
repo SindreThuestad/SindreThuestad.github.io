ab75e860a0d245446316ada3f3a23466a4d42083
SindreThuestad <woordclan@hotmail.com>
2018-03-08 22:36:08 +0100

    damn

---
+++ rapidtech/app.js
@@ -0,0 +1,59 @@
+(function () {
+
+    var config = {
+        apiKey: "AIzaSyDUChLfZSVKNoENHM0OUrBODgzCaefP7KU",
+        authDomain: "rapidtech-bfde6.firebaseapp.com",
+        databaseURL: "https://rapidtech-bfde6.firebaseio.com",
+        projectId: "rapidtech-bfde6",
+        storageBucket: "rapidtech-bfde6.appspot.com",
+        messagingSenderId: "624808561949"
+    };
+    firebase.initializeApp(config);
+
+
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const txtLogin = document.getElementById('txtLogin');
+    const txtSignUp = document.getElementById('txtSignUp');
+    const txtLogout = document.getElementById('txtLogout');
+
+
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(email,
+        pass);
+        promise.catch(e => console.log(e.message));
+    });
+
+    btnSignUp.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        const promise = auth.createUserWithEmailAndPassword(email,
+        pass);
+        promise
+        .catch(e => console.log(e.message));
+    });
+
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+
+    });
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+            btnLogout.classList.remove('hide');
+        } else {
+            console.log('not logged in');
+            btnLogout.classList.add('hide');
+        }
+    });
+
+
+
+
+}());
+
---
+++ rapidtech/pc.js
@@ -0,0 +1,38 @@
+
+function toggle_1 (id) {
+
+    var divelement = document.getElementById (id);
+
+    if(divelement.style.display == 'none')
+       divelement.style.display = 'block';
+    
+    else
+       divelement.style.display = 'none';
+       
+}
+
+
+function toggle_2 (id) {
+
+    var divelement = document.getElementById (id);
+
+    if(divelement.style.display == 'none')
+       divelement.style.display = 'block';
+    
+    else
+       divelement.style.display = 'none';
+       
+}
+
+
+function toggle_3 (id) {
+
+    var divelement = document.getElementById (id);
+
+    if(divelement.style.display == 'none')
+       divelement.style.display = 'block';
+    
+    else
+       divelement.style.display = 'none';
+       
+}
